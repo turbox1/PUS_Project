26f2a3f38f0c80ce0a79e91de8e06ba07ac16556
Jarek Kostrz <jkostrz@gmail.com>
2012-03-25 13:00:19 +0200

    Javascript start application, autoupdating temperature

--- VSCPc/src/vscpc.c
+++ VSCPc/src/vscpc.c
@@ -9,14 +9,11 @@
 #define CLASS "\"class\""
 #define TYPE "\"type\""
 #define ID "\"id\""
-#define PARAMS "\"params\""
+#define OPTIONS "\"options\""
 #define VAL "\"value\""
-#define UNIT "\"unit\""
-
 
 //--- function ---
 void print_all_frame(const int dev_numb, struct vscp_frame* vf);
-char* give_params(struct vscp_frame* vf);
 
 
 int main() {
@@ -74,29 +71,8 @@ void print_all_frame(const int dev_numb, struct vscp_frame* vf) {
   printf("[");
   int i;
   for(i=0; i<dev_numb; i++) {
-    char* ttmp = give_params(&vf[i]);
-    printf("{%s: \"%d\", %s: \"%d\", %s: \"%d\", %s: {%s}}", CLASS, vf[i].v_class, TYPE, vf[i].v_type, ID, vf[i].v_data[7], PARAMS, ttmp);
+    printf("{%s: \"%d\", %s: \"%d\", %s: \"%d\", %s: \"%d\"}", CLASS, vf[i].v_class, TYPE, vf[i].v_type, ID, vf[i].v_data[7], VAL, vf[i].v_data[3]);
     if(i<dev_numb-1) printf(", ");
-    free(ttmp);
   }
   printf("]");
-}
-
-char* give_params(struct vscp_frame* vf) {
-  char *tmp = (char*)malloc(128*sizeof(unsigned char));
-  if(vf->v_class == 10) {
-    int i;
-    int val;
-    switch(vf->v_type) {
-    case 6:
-      for(i=0; i<4; i++) ((uint8_t*)&val)[i] = vf->v_data[3-i];
-      sprintf(tmp, "%s: \"%d\", %s: \"%d\"", VAL, val, UNIT, 0);
-      break;
-    
-    case 35:
-      sprintf(tmp, "%s: \"%d\"", VAL, val);
-      break;
-    }
-  }
-  return tmp;
 }
--- www/test.js
+++ www/test.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
     $('#send-link').click(function() {
         $.ajax({
-            //url: 'cgi-bin/vscpc.cgi',
-            url: 'test.json',
+            url: 'cgi-bin/vscpc.cgi',
+           // url: 'test.json',
+            dataType: 'json',
+            data: "class=20&type=10&data=222",
             success: function(data) {
-                var a = $.parseJSON(data);
+                var a = data;
                 $('#class').append(a.class);
                 $('#type').append(a.type);
                 $('#data').append(a.data);
@@ -13,3 +15,87 @@ $(document).ready(function() {
         return false;
     });
 });
+
+
+var App = {
+    
+    options: {},
+
+    run: function() {
+        var self = this;
+        self.Deamon().run();
+    }
+
+};
+
+App.Deamon = function() {
+    
+    var _options = {
+        url     : "cgi-bin/vscpc.cgi",
+        data    : "class=15&type=0",
+        interval: 10000
+    };
+
+    var run = function() {
+        var self = this;
+        (function worker() {
+                $.ajax({
+                    url: self.getOptions.url,
+                    dataType: 'json',
+                    data: self.getOptions.data,
+                    success: function(response) {
+                        App.Builder.update(response);
+                    },
+                    complete: function() {
+                        setTimeout(worker, self.getOptions.interval);
+                    }
+                });
+        })();
+    }
+
+    return {
+        run: run,
+        getOptions: _options
+    }
+};
+
+App.Builder = (function() { 
+
+    var _devicesDatabase;
+    
+    function _addData(data) {
+        _devicesDatabase = data;
+    };
+
+    function _refreshAll() {
+        $(_devicesDatabase).each(function() {
+            if (this.type == 6) {
+                $('#class').html(this.class);
+                $('#type').html(this.type);
+                $('#id').html(this.id);
+                $('#value').html(this.params.value);
+                var unit;
+                switch (parseInt(this.params.unit)) {
+                    case 0: unit = "K";
+                    case 1: unit = "C";
+                    case 2: unit = "F";    
+                }
+                $('#unit').html(unit);
+            }
+        });
+    };
+
+    return {
+        update: function(data) {
+            _addData(data);
+            _refreshAll(); 
+        }
+    }
+
+}());
+
+
+var app = App;
+app.run(); 
+
+
